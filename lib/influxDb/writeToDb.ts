6939cbd0aabf9e4f987323356f5697eb24f7c097
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2021-05-16 17:28:01 -0700

    add dev script

--- lib/influxDb/writeToDb.ts
+++ lib/influxDb/writeToDb.ts
@@ -13,6 +13,10 @@ const client = new InfluxDB({
 const writeApi = client.getWriteApi(org, bucket);
 writeApi.useDefaultTags({ host: config.influxDbHostName });
 
+if (process.env.TS_NODE_DEV == "true") {
+  console.warn("In development mode, not sending anything to InfluxDB.");
+}
+
 export default function writeToDb(
   spotifyData: NowPlayingTrack,
   trackFeatures: TrackFeatures,
@@ -25,42 +29,44 @@ export default function writeToDb(
     spotifyData.item.popularity,
     timestamp
   );
-  const point = new Point("track")
-    .intField("seconds_played", duration) // .intField("playing", playing)
-    .tag("device_id", spotifyData.device.id)
-    .tag("device_name", spotifyData.device.name)
-    .tag("device_type", spotifyData.device.type)
-    .intField("device_volume_percent", spotifyData.device.volume_percent)
-    .tag("shuffle_state", spotifyData.shuffle_state.toString())
-    .tag("repeat_state", spotifyData.repeat_state)
-    .tag("album_type", spotifyData.item.album.album_type)
-    .tag("album_id", spotifyData.item.album.id)
-    .tag("album_name", spotifyData.item.album.name)
-    // .stringField("album_release_date", spotifyData.item.album.release_date)
-    // .tag(
-    //   "release_date_precision",
-    //   spotifyData.item.album.release_date_precision
-    // )
-    .intField("album_total_tracks", spotifyData.item.album.total_tracks)
-    .tag("explicit", spotifyData.item.explicit.toString())
-    .tag("track_id", spotifyData.item.id)
-    .tag("track_name", spotifyData.item.name)
-    .tag("artist_id", spotifyData.item.artists[0].id)
-    .tag("artist_name", spotifyData.item.artists[0].name)
-    .floatField("danceability", trackFeatures.danceability)
-    .floatField("energy", trackFeatures.energy)
-    .tag("key", trackFeatures.key.toString())
-    .floatField("loudness", trackFeatures.loudness)
-    .tag("mode", trackFeatures.mode.toString())
-    .floatField("speechiness", trackFeatures.speechiness)
-    .floatField("acousticness", trackFeatures.acousticness)
-    .floatField("instrumentalness", trackFeatures.instrumentalness)
-    .floatField("liveness", trackFeatures.liveness)
-    .floatField("valence", trackFeatures.valence)
-    .floatField("tempo", trackFeatures.tempo)
-    .intField("duration_ms", trackFeatures.duration_ms)
-    .tag("time_signature", trackFeatures.time_signature.toString())
-    .intField("popularity", spotifyData.item.popularity)
-    .timestamp(timestamp);
-  writeApi.writePoint(point);
+  if (process.env.TS_NODE_DEV != "true") {
+    const point = new Point("track")
+      .intField("seconds_played", duration) // .intField("playing", playing)
+      .tag("device_id", spotifyData.device.id)
+      .tag("device_name", spotifyData.device.name)
+      .tag("device_type", spotifyData.device.type)
+      .intField("device_volume_percent", spotifyData.device.volume_percent)
+      .tag("shuffle_state", spotifyData.shuffle_state.toString())
+      .tag("repeat_state", spotifyData.repeat_state)
+      .tag("album_type", spotifyData.item.album.album_type)
+      .tag("album_id", spotifyData.item.album.id)
+      .tag("album_name", spotifyData.item.album.name)
+      // .stringField("album_release_date", spotifyData.item.album.release_date)
+      // .tag(
+      //   "release_date_precision",
+      //   spotifyData.item.album.release_date_precision
+      // )
+      .intField("album_total_tracks", spotifyData.item.album.total_tracks)
+      .tag("explicit", spotifyData.item.explicit.toString())
+      .tag("track_id", spotifyData.item.id)
+      .tag("track_name", spotifyData.item.name)
+      .tag("artist_id", spotifyData.item.artists[0].id)
+      .tag("artist_name", spotifyData.item.artists[0].name)
+      .floatField("danceability", trackFeatures.danceability)
+      .floatField("energy", trackFeatures.energy)
+      .tag("key", trackFeatures.key.toString())
+      .floatField("loudness", trackFeatures.loudness)
+      .tag("mode", trackFeatures.mode.toString())
+      .floatField("speechiness", trackFeatures.speechiness)
+      .floatField("acousticness", trackFeatures.acousticness)
+      .floatField("instrumentalness", trackFeatures.instrumentalness)
+      .floatField("liveness", trackFeatures.liveness)
+      .floatField("valence", trackFeatures.valence)
+      .floatField("tempo", trackFeatures.tempo)
+      .intField("duration_ms", trackFeatures.duration_ms)
+      .tag("time_signature", trackFeatures.time_signature.toString())
+      .intField("popularity", spotifyData.item.popularity)
+      .timestamp(timestamp);
+    writeApi.writePoint(point);
+  }
 }
